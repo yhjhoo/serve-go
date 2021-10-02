a97c397bd31348081c4579fca6727dadd4117672
yhjhoo <yhjhoo@gmail.com>
2021-10-02 12:47:01 +0800

    Dynamic folder path

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("sample/html"))
+	folderPath := flag.String("folder", ".", "folder path")
+	fs := http.FileServer(http.Dir(*folderPath))
 
 	http.Handle("/", fs)
 
-	log.Println("listen on port 8888")
+	log.Println("listen on port 8888, folderPath:" + *folderPath)
 	log.Println("http://localhost:8888")
 
 	if err := http.ListenAndServe(":8888", nil); err != nil {
