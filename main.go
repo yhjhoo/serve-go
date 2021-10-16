af1c0c50655df4bfa96c72d90a70a7a9dd3b1d65
yhjhoo <yhjhoo@gmail.com>
2021-10-16 16:28:11 +0800

    Add upload endpoint and html

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"flag"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -19,10 +24,31 @@ func main() {
 
 	http.Handle("/", fs)
 
+	http.HandleFunc("/upload", func(rw http.ResponseWriter, r *http.Request) {
+		filePartBody, _ := ioutil.ReadAll(r.Body)
+		var filePart FilePart
+		json.Unmarshal(filePartBody, &filePart)
+		println("=================================fileName: " + filePart.FileName)
+		println("===" + filePart.Content)
+
+		filePart.Content = strings.Replace(filePart.Content, "data:application/octet-stream;base64,", "", 1)
+		content, _ := base64.URLEncoding.DecodeString(filePart.Content)
+
+		println("content: " + string(content))
+
+		file, _ := os.OpenFile(filePart.FileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		file.Write(content)
+	})
+
 	log.Println("listen on port 8888, folder:" + folderPath)
 	log.Println("http://localhost:8888")
 
 	if err := http.ListenAndServe(":8888", nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+type FilePart struct {
+	FileName string `json:"fileName"`
+	Content  string `json:"content"`
 }
